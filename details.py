38a91dc05acbf5572847d813ba13a27b2ae6a363
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-04 15:02:54 +0100

    Updates details.py to find table of details

--- details.py
+++ details.py
@@ -4,5 +4,12 @@ from bs4 import BeautifulSoup
 test_link = "https://www.amazon.co.uk/Razer-Blade-17-Display-Chamber/dp/B09Q6CVSW3/ref=sr_1_3?keywords=laptop&qid=1664886688&qu=eyJxc2MiOiI5LjQzIiwicXNhIjoiOC44MSIsInFzcCI6IjguMTkifQ%3D%3D&sr=8-3"
 
 html_request = requests.get(test_link)
-html_string = html_request.text
-print(html_string)
+html_string = html_request.content
+soup = BeautifulSoup(html_string, 'html.parser')
+# print(soup.prettify())
+
+print(soup)
+
+details = soup.find("table", {"id": "productDetails_techSpec_section_1"})
+
+print(details)
