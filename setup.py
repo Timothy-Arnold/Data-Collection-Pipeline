614ff674104cb4fc4411bf46149dc1eee83a660e
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-08 22:47:21 +0100

    Adds setup folder for future publishing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "amazon_laptop_scraper"
+    version = "0.0.1"
+    description = "Package that scrapes the information of the first 20 pages of laptops from searching in Amazon"
+    url = "tbd"
+    author = "Timothy Arnold"
+    license = "tbd"
+    packages = find_packages()
+)
