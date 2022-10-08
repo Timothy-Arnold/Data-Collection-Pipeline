118afcd924b04c01eba223801182583d62b25495
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-08 21:47:06 +0100

    Adds additional unit-test for website URLs

--- test/test_scraper.py
+++ test/test_scraper.py
@@ -2,7 +2,7 @@ from project.scraper import Scraper
 import unittest
 
 class ScraperTestCase(unittest.TestCase): 
-    
+
     def setUp(self, URL = "https://www.amazon.co.uk/"):
         scrape = Scraper()     
         self.link_list = scrape.scrape_all()
@@ -11,6 +11,8 @@ class ScraperTestCase(unittest.TestCase):
         minimum_length = 200
         actual_length = len(self.link_list)
         self.assertGreaterEqual(actual_length, minimum_length)
+        non_website_list = list(filter(lambda x: x[:8] != 'https://', self.link_list))
+        self.assertEqual(len(non_website_list), 0)
 
 if __name__ == '__main__':   
     unittest.main()
