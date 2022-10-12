36648c36e92765fc7a4af1ee6bd75ef3d9f3dfb0
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-12 23:05:15 +0100

    Refactors details.py to take details of laptops on box.co.uk

--- project/details.py
+++ project/details.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import uuid
 
@@ -34,37 +35,45 @@ class Details:
         self.URL = URL
         self.driver.get(URL)
         time.sleep(1)
-        self.required_details = ["Price", "Brand", "Standing screen display size", "Screen Resolution", "RAM Size", "Item Weight", "ASIN", "Image", "UUID"]
-        self.initial_values = ["Unknown"] * 9
-        self.details_dict = {self.required_details[i]: self.initial_values[i] for i in range(9)}
+        self.required_details = ["Price", "Screen Size", "Screen Resolution", "Total Storage", "RAM", "Stock Code", "Image", "UUID"]
+        self.initial_values = ["Unknown"] * 8
+        self.details_dict = {self.required_details[i]: self.initial_values[i] for i in range(8)}
 
-    def __extract_text_data(self):
-        if EC.presence_of_element_located((By.XPATH, '//*[@class="a-alert-content"]')):
-            self.driver.get(self.URL)
-            time.sleep(1)
-        
-        price_location = self.driver.find_element(By.XPATH, '//*[@id="corePriceDisplay_desktop_feature_div"]')
-        price_pound = price_location.find_element(By.XPATH, '//span[@class="a-price-whole"]').text
-        price_penny = price_location.find_element(By.XPATH, '//span[@class="a-price-fraction"]').text
-        price = f"£{price_pound}.{price_penny}"
+    def __extract_price(self):
+        price = self.driver.find_element(By.XPATH, '//span[@class="pq-price"]').text
+        return price
 
-        self.details_dict["Price"] = price
+    def __click_specifications(self):
+        self.driver.find_element(by=By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
+        time.sleep(1)
+        spec_button = self.driver.find_element(By.XPATH, '//p[@data-id = "Specifications"]')
+        spec_button.click()
+        time.sleep(1)
 
-        details_table = self.driver.find_element(By.XPATH, '//*[@id="productDetails_techSpec_section_1"]')
-        name_list = details_table.find_elements(By.XPATH, '//th[@class = "a-color-secondary a-size-base prodDetSectionEntry"]')
-        value_list = details_table.find_elements(By.XPATH, '//td[@class = "a-size-base prodDetAttrValue"]')
-        
-        for index_1 in range(len(name_list)):
-            if name_list[index_1].text == "Resolution":
-                self.details_dict["Screen Resolution"] = value_list[index_1].text
-            for index_2 in range(1,7):
-                if name_list[index_1].text == self.required_details[index_2]:
-                    self.details_dict[self.required_details[index_2]] = value_list[index_1].text
+    def __extract_technical_data(self):
+
+        details_table = self.driver.find_element(By.XPATH, '//*[@id="p-specifications"]')
+        specs = details_table.find_elements(By.XPATH, '//td[@class = "speccol"]')
+        names = specs[::2]
+        values = specs[1::2]
+        for index_1 in range(len(names)):
+            if names[index_1].text == "Screen":
+                screen_specs = values[index_1].text
+                screen_size = screen_specs.split('"')[0]
+                self.details_dict["Screen Size"] = screen_size + " Inches"
+            for index_2 in range(2,5):
+                if names[index_1].text == self.required_details[index_2]:
+                    self.details_dict[self.required_details[index_2]] = values[index_1].text
 
         return self.details_dict
+    
+    def __extract_stock_code(self):
+        stock_code_string = self.driver.find_element(By.XPATH, '//p[@class="p-reference p-mancode"]').text
+        stock_code = stock_code_string[stock_code_string.index(':') + 2: stock_code_string.index('|') - 1]
+        return stock_code
 
     def __extract_img_data(self):
-        img_location = self.driver.find_element(By.XPATH, '//*[@id="landingImage"]')
+        img_location = self.driver.find_element(By.XPATH, '//img[@class="p-image-button pq-images-small pq-images-show"]')
         img = img_location.get_attribute("src")
         return img
 
@@ -73,14 +82,16 @@ class Details:
         return UUID
 
     def extract_all_data(self):
-        self.details_dict = Details.__extract_text_data(self)
+        self.details_dict["Price"] = Details.__extract_price(self)
+        Details.__click_specifications(self)
+        self.details_dict = Details.__extract_technical_data(self)
+        self.details_dict["Stock Code"] = Details.__extract_stock_code(self)
         self.details_dict["Image"] = Details.__extract_img_data(self)
         self.details_dict["UUID"] = Details.__assign_uuid(self)
         return self.details_dict
 
-test_URL = "https://www.amazon.co.uk/CHUWI-GemiBook-2160x1440-Intel-Processor/dp/B08NPCBGSZ/ref=sr_1_12?crid=3E23J6SE3Y156&keywords=laptop&qid=1665235352&qu=eyJxc2MiOiI5LjQzIiwicXNhIjoiOC44MSIsInFzcCI6IjguMTkifQ%3D%3D&sprefix=laptop%2Caps%2C95&sr=8-12"
-
 if __name__ == '__main__':
+    test_URL = "https://www.box.co.uk/9S7-14C612-043-MSI-Prestige-14Evo-Intel-Core-i7-16GB-RA_4110589.html"
     extraction = Details(test_URL)
     extraction.extract_all_data()
     print(extraction.details_dict)
