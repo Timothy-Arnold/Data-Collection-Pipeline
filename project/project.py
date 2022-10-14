aa8dd551857d86abb44cf2e4a5909224fabda6b3
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-14 02:26:49 +0100

    Changes Final message

--- project/project.py
+++ project/project.py
@@ -8,7 +8,7 @@ def do_full_scrape():
     scrape = Scraper()
     link_list = scrape.scrape_all()
     print(link_list)
-    time.sleep(30)
+    time.sleep(2)
     # Initialize list of detail_dict's
     details_dict_list = []
     driver = webdriver.Chrome()
@@ -22,16 +22,10 @@ def do_full_scrape():
             details_dict_list.append(details_dict)
         except:
             pass
-    number_of_products = len(details_dict_list)
-    print(f"{number_of_products} laptops have been scraped")
     for details_dict in details_dict_list:
         store = Storage(details_dict)
         store.download_all_data()
-    print("All Finished!")
+    print("All downloaded!")
 
 if __name__ == '__main__':
     do_full_scrape()
-
-# import boto3
-
-# s3 = boto3.client("s3")
