a1a7f34078982fe56f991e0cec8393664300d43c
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-21 16:00:44 +0100

    Adds docstring to function

--- project/project.py
+++ project/project.py
@@ -1,12 +1,26 @@
+import boto3
+from details import Details
+from scraper import Scraper
+from data_storage import Storage
 import time
 from selenium import webdriver
-from scraper import Scraper
-from details import Details
-from data_storage import Storage
-import boto3
 
 def do_full_scrape():
-    scrape = Scraper()
+    '''
+    Scrapes laptops from the box website.
+    
+    This function scrapes the first 15 pages of laptops' product page urls on the box website, then accesses these URLs to get the information of each laptop.
+    It then stores this technical and image data in my local directory, as well as uploads it to an S3 bucket.
+
+    Parameters
+    ----------
+    None
+    
+    Returns
+    ----------
+    None
+    '''
+    scrape = Scraper(2)
     link_list = scrape.scrape_all()
     print(link_list)
     time.sleep(2)
