54f36e909538c63b8056bc3bb158ccef385aa519
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-10-28 17:34:03 +0100

    Adds extra comments

--- docker/project.py
+++ docker/project.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -20,14 +21,29 @@ def do_full_scrape(pages=15):
     ----------
     None
     '''
+    # Getting laptop links
+    print("Scraping links")
     scrape = Scraper(pages)
     link_list = scrape.scrape_all()
-    print(link_list)
     link_list_length = len(link_list)
     print(f"{link_list_length} links have been scraped")
     time.sleep(2)
-    # Initialize list of detail_dict's
+
+    subprocess.run("systemctl enable --now warp-svc.service", shell=True, stderr=subprocess.STDOUT, timeout=60)
+    # Start WARP
+    subprocess.run("systemctl start warp-svc.service", shell=True, stderr=subprocess.STDOUT)
+    subprocess.run("systemctl status warp-svc.service", shell=True, stderr=subprocess.STDOUT)
+    # First register the warp-cli
+    subprocess.run("warp-cli --accept-tos register", shell=True, stderr=subprocess.STDOUT, timeout=60)
+    subprocess.run("warp-cli --accept-tos register", shell=True, stderr=subprocess.STDOUT, timeout=60)
+    # First connect to WARP
+    subprocess.run("warp-cli --accept-tos connect", shell=True, stderr=subprocess.STDOUT)
+    print(subprocess.run("warp-cli --accept-tos status", shell=True, stderr=subprocess.STDOUT))
+    print("WARP started!")
+
+    # Gettings Details
     details_dict_list = []
+    link_list_index = 0
     chromeOptions = Options()
     chromeOptions.headless = True
     chromeOptions.add_argument("window-size=1920x1080")
@@ -35,7 +51,7 @@ def do_full_scrape(pages=15):
     chromeOptions.add_argument('--disable-gpu')
     chromeOptions.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chromeOptions)
-    for link in link_list[:5]:
+    for link in link_list:
         driver.get(link)
         time.sleep(3)
         try:
@@ -46,12 +62,23 @@ def do_full_scrape(pages=15):
         except:
             print("Page didn't work")
             pass
+        link_list_index += 1
+        if link_list_index % 10 == 0:
+            print("Restarting WARP")
+            subprocess.run("warp-cli --accept-tos disconnect", shell=True, stderr=subprocess.STDOUT)
+            print(subprocess.run("warp-cli --accept-tos status", shell=True, stderr=subprocess.STDOUT))
+            time.sleep(3)
+            subprocess.run("warp-cli --accept-tos connect", shell=True, stderr=subprocess.STDOUT)
+            print(subprocess.run("warp-cli --accept-tos status", shell=True, stderr=subprocess.STDOUT))
+            pass
     number_of_products = len(details_dict_list)
     print(f"{number_of_products} laptops have been scraped")
+
+    #Storage
     for details_dict in details_dict_list:
         store = Storage(details_dict)
         store.download_all_data()
-    print("All downloaded and uploaded!")
+    print("All downloaded!")
 
 if __name__ == '__main__':
     do_full_scrape(2)
