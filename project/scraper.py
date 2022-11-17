248555a78b9e2f71ab1b21d4a24cf8daae3bbb65
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-11-17 16:40:30 +0000

    Adds method to click load more in Scraper

--- project/scraper.py
+++ project/scraper.py
@@ -30,41 +30,43 @@ class Scraper:
     scrape_all()
         Collects the product links from all 15 pages on box and stores them in the list.
     '''
-    def __init__(self, number_of_pages=15):
+    def __init__(self, number_of_pages=6):
         chromeOptions = Options()
-        chromeOptions.headless = True
+        chromeOptions.headless = False
         self.driver = webdriver.Chrome(options=chromeOptions)
         self.link_list = []
         self.number_of_pages = number_of_pages
-        time.sleep(1)
+        time.sleep(2)
 
     def __open_webpage(self):
-        self.driver.get("https://www.box.co.uk/laptops")
+        self.driver.get("https://www.rottentomatoes.com/browse/movies_at_home/sort:popular?page=5")
+        time.sleep(2)
+
+    def __click_load_more(self):
+        load_more_button = self.driver.find_element(By.XPATH, '//button[contains(text(), "Load more")]')
+        load_more_button.click()
         time.sleep(2)
 
     def __get_links(self):
-        product_container = self.driver.find_element(By.XPATH, '//div[@class = "product-list  "]')
-        product_list_1 = product_container.find_elements(By.XPATH, '//div[@class = "product-list-item "]')
-        product_list_2 = product_container.find_elements(By.XPATH, '//div[@class = "product-list-item middle"]')
-        product_list = product_list_1 + product_list_2
+        print("Getting Links!")
+        product_container = self.driver.find_element(By.XPATH, '//div[@class = "discovery-tiles"]')
+        product_list = product_container.find_elements(By.XPATH, '//a[@class = "js-tile-link"]')
         for product in product_list:
-            a_tag = product.find_element(By.TAG_NAME, 'a')
-            link = a_tag.get_attribute("href")
+            # a_tag = product.find_element(By.TAG_NAME, 'a')
+            link = product.get_attribute("href")
             self.link_list.append(link)
+        print("Links gotten!")
+        time.sleep(2)
 
     def __go_to_page(self, page_number):
-        page_url = f"https://www.box.co.uk/laptops/page/{page_number}"
+        page_url = f"https://www.laptopsdirect.co.uk/ct/laptops-and-netbooks/laptops?pageNumber={page_number}"
         self.driver.get(page_url)
         time.sleep(1)
 
     def scrape_all(self):
         Scraper.__open_webpage(self)
-        # Get the first page's links
+        Scraper.__click_load_more(self)
         Scraper.__get_links(self)
-        for page_number in range(2, self.number_of_pages + 1):
-            Scraper.__go_to_page(self, page_number)
-            Scraper.__get_links(self)
-        self.driver.quit()
         return self.link_list
 
 if __name__ == '__main__':
